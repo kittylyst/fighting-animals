1b3c30521ce704ab7fc8f1739dcde36d9dd2ed55
Shaaf, Syed <sshaaf@redhat.com>
2022-10-03 06:22:33 +0200

    add OTel jar download to sh and README

--- make.sh
+++ make.sh
@@ -1,13 +1,18 @@
-#This Docker file helps to run a animal service
+#This make file builds the docker images locally
 
 #To build a package
+mvn clean package
 
-mvn package
+# Get the Otel agent jar
+wget https://github.com/open-telemetry/opentelemetry-java-instrumentation/releases/latest/download/opentelemetry-javaagent.jar && mv opentelemetry-javaagent.jar target/
 
 #To build a image
-
-docker build -t animalservice
+docker build -t animals_demo -f src/main/docker/animal/Dockerfile target/
+docker build -t feline_demo -f src/main/docker/feline/Dockerfile target/
+docker build -t fish_demo -f src/main/docker/fish/Dockerfile target/
+docker build -t mammal_demo -f src/main/docker/mammal/Dockerfile target/
+docker build -t mustelid_demo -f src/main/docker/mustelid/Dockerfile target/
 
 #List a images
+docker images "*demo"
 
-docker images
