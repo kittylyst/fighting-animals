c08fe5b0111c8c2972d327f7be750e8a10de777d
Ben Evans <benjamin.john.evans@gmail.com>
2023-06-05 22:04:42 +0200

    Initial wirng

--- src/main/java/io/opentelemetry/examples/animal/AnimalApplication.java
+++ src/main/java/io/opentelemetry/examples/animal/AnimalApplication.java
@@ -1,11 +1,19 @@
 /* Copyright (C) Red Hat 2023 */
 package io.opentelemetry.examples.animal;
 
+import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.simple.SimpleMeterRegistry;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class AnimalApplication {
+
+  @Bean
+  public MeterRegistry basicRegistry() {
+    return new SimpleMeterRegistry();
+  }
 
   public static void main(String[] args) {
     SpringApplication.run(AnimalApplication.class, args);
--- src/main/java/io/opentelemetry/examples/animal/AnimalController.java
+++ src/main/java/io/opentelemetry/examples/animal/AnimalController.java
@@ -3,9 +3,11 @@ package io.opentelemetry.examples.animal;
 
 import static io.opentelemetry.examples.utils.Misc.fetchAnimal;
 
+import io.micrometer.core.instrument.MeterRegistry;
 import java.io.IOException;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
 import javax.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,11 +22,21 @@ public class AnimalController {
 
   @Autowired private HttpServletRequest httpServletRequest;
 
+  private final AtomicInteger battlesTotal;
+
+  private final MeterRegistry registry;
+
+  public AnimalController(MeterRegistry registry) {
+    this.registry = registry;
+    this.battlesTotal = this.registry.gauge("battles.total", new AtomicInteger(0));
+  }
+
   @GetMapping("/battle")
   public String makeBattle() throws IOException, InterruptedException {
-    // Send the two requests and return the response body as the response, and end the span.
+    // Send the two requests and return the response body as the response
     var good = fetchRandomAnimal();
     var evil = fetchRandomAnimal();
+    battlesTotal.incrementAndGet();
     return "{ \"good\": \"" + good + "\", \"evil\": \"" + evil + "\" }";
   }
 
