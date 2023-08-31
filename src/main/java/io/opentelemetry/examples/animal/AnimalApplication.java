bdba7ae7b08f8d725549329e7be2fe8144a8c77a
Ben Evans <benjamin.john.evans@gmail.com>
2023-08-31 08:31:49 +0200

    Strip vestiges of metrics, pin collector version

--- src/main/java/io/opentelemetry/examples/animal/AnimalApplication.java
+++ src/main/java/io/opentelemetry/examples/animal/AnimalApplication.java
@@ -1,27 +1,11 @@
 /* (C)2022-2023 */
 package io.opentelemetry.examples.animal;
 
-import io.micrometer.core.instrument.MeterRegistry;
-import io.micrometer.core.instrument.Metrics;
-import java.util.Optional;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class AnimalApplication {
-
-  @Bean
-  @ConditionalOnClass(name = "io.opentelemetry.javaagent.OpenTelemetryAgent")
-  public MeterRegistry otelRegistry() {
-    Optional<MeterRegistry> otelRegistry =
-        Metrics.globalRegistry.getRegistries().stream()
-            .filter(r -> r.getClass().getName().contains("OpenTelemetryMeterRegistry"))
-            .findAny();
-    otelRegistry.ifPresent(Metrics.globalRegistry::remove);
-    return otelRegistry.orElse(null);
-  }
 
   public static void main(String[] args) {
     SpringApplication.run(AnimalApplication.class, args);
