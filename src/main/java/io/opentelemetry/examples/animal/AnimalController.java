2f1e1dd094c862469554a81517b07401c4a2deb6
Ben Evans <beevans@redhat.com>
2023-10-26 17:35:51 +0200

    Add Timer example

--- src/main/java/io/opentelemetry/examples/animal/AnimalController.java
+++ src/main/java/io/opentelemetry/examples/animal/AnimalController.java
@@ -5,11 +5,13 @@ import static io.opentelemetry.examples.utils.Misc.fetchAnimal;
 
 import io.micrometer.core.instrument.Counter;
 import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.Timer;
 import io.micrometer.core.instrument.binder.jvm.JvmMemoryMetrics;
 import io.micrometer.core.instrument.binder.system.ProcessorMetrics;
 import java.io.IOException;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.Callable;
 import javax.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -26,23 +28,33 @@ public class AnimalController {
 
   private final Counter battlesTotal;
 
+  private final Timer responseTimer;
+
   private final MeterRegistry registry;
 
   public AnimalController(MeterRegistry registry) {
     this.registry = registry;
     this.battlesTotal = this.registry.counter("battles.total");
+    this.responseTimer = Timer
+            .builder("response.time")
+            .description("Response time")
+            .register(registry);
+
     // These next two lines switch on CPU & memory metrics for delivery through Micrometer
     new ProcessorMetrics().bindTo(this.registry);
     new JvmMemoryMetrics().bindTo(this.registry);
   }
 
   @GetMapping("/battle")
-  public String makeBattle() throws IOException, InterruptedException {
-    // Send the two requests and return the response body as the response
-    var good = fetchRandomAnimal();
-    var evil = fetchRandomAnimal();
+  public String makeBattle() throws Exception {
+    Callable<String> callable = () -> {
+      // Send the two requests and return the response body as the response
+      var good = fetchRandomAnimal();
+      var evil = fetchRandomAnimal();
+      return "{ \"good\": \"" + good + "\", \"evil\": \"" + evil + "\" }";
+    };
     battlesTotal.increment();
-    return "{ \"good\": \"" + good + "\", \"evil\": \"" + evil + "\" }";
+    return responseTimer.recordCallable(callable);
   }
 
   private String fetchRandomAnimal() throws IOException, InterruptedException {
