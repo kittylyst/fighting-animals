a7098f39f6600c17bc86a40606fad653343510a5
Ben Evans <beevans@redhat.com>
2024-01-05 12:58:17 +0100

    Take 2

--- src/main/java/io/opentelemetry/examples/animal/AnimalApplication.java
+++ src/main/java/io/opentelemetry/examples/animal/AnimalApplication.java
@@ -1,19 +1,11 @@
 /* Copyright (C) Red Hat 2023-2024 */
 package io.opentelemetry.examples.animal;
 
-import io.micrometer.core.instrument.MeterRegistry;
-import io.micrometer.core.instrument.logging.LoggingMeterRegistry;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class AnimalApplication {
-
-  @Bean
-  public MeterRegistry basicRegistry() {
-    return new LoggingMeterRegistry();
-  }
 
   public static void main(String[] args) {
     SpringApplication.run(AnimalApplication.class, args);
--- src/main/java/io/opentelemetry/examples/fish/FishApplication.java
+++ src/main/java/io/opentelemetry/examples/fish/FishApplication.java
@@ -1,18 +1,11 @@
 /* Copyright (C) Red Hat 2023-2024 */
 package io.opentelemetry.examples.fish;
 
-import io.micrometer.core.instrument.MeterRegistry;
-import io.micrometer.core.instrument.logging.LoggingMeterRegistry;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class FishApplication {
-  @Bean
-  public MeterRegistry basicRegistry() {
-    return new LoggingMeterRegistry();
-  }
 
   public static void main(String[] args) {
     SpringApplication.run(FishApplication.class, args);
