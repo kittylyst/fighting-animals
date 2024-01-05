23d24686c3cbf2c6adfa1b9b13431dbc3ba3bcb8
Ben Evans <beevans@redhat.com>
2024-01-05 19:02:56 +0100

    Fix exporting endpoints

--- make.sh
+++ make.sh
@@ -2,12 +2,13 @@
 
 #To build a package
 
-mvn package
+#mvn package
 
 #To build a image
 
-docker build -t animalservice
+docker build  -t animals_demo -f src/main/docker/animal/Dockerfile .
+docker build  -t fish_demo -f src/main/docker/fish/Dockerfile .
+docker build  -t mustelid_demo -f src/main/docker/mustelid/Dockerfile .
+docker build  -t feline_demo -f src/main/docker/feline/Dockerfile .
+docker build  -t mammal_demo -f src/main/docker/mammal/Dockerfile .
 
-#List a images
-
-docker images
