405131d2d47a7f176111ffee198541c680aaf78e
Ben Evans <beevans@redhat.com>
2024-01-05 12:20:05 +0100

    Move to a working build

--- make.sh
+++ make.sh
@@ -1,4 +1,17 @@
-# Build Images
+#This Docker file helps to run a animal service
 
-docker build -t animalservice
+#To build a package
 
+#mvn package
+
+#To build a image
+
+docker build  -t animals_demo -f src/main/docker/animal/Dockerfile .
+docker build  -t fish_demo -f src/main/docker/fish/Dockerfile .
+docker build  -t mustelid_demo -f src/main/docker/mustelid/Dockerfile .
+docker build  -t feline_demo -f src/main/docker/feline/Dockerfile .
+docker build  -t mammal_demo -f src/main/docker/mammal/Dockerfile .
+
+#List a images
+
+# docker images
