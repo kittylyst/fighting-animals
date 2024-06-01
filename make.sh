47b9a8bdc9c94ccce6df47943ae9f8df41b700f3
Ben Evans <beevans@redhat.com>
2024-06-01 18:37:41 +0200

    Working state

--- make.sh
+++ make.sh
@@ -1,6 +1,9 @@
+# To build the Maven jar
 mvn clean package
-docker build -t animals_demo -f src/main/docker/animal/Dockerfile target/
-docker build -t feline_demo -f src/main/docker/feline/Dockerfile target/
-docker build -t fish_demo -f src/main/docker/fish/Dockerfile target/
-docker build -t mammal_demo -f src/main/docker/mammal/Dockerfile target/
-docker build -t mustelid_demo -f src/main/docker/mustelid/Dockerfile target/
+
+# Build the containers
+docker build  -t animals_demo -f src/main/docker/animal/Dockerfile .
+docker build  -t fish_demo -f src/main/docker/fish/Dockerfile .
+docker build  -t mustelid_demo -f src/main/docker/mustelid/Dockerfile .
+docker build  -t feline_demo -f src/main/docker/feline/Dockerfile .
+docker build  -t mammal_demo -f src/main/docker/mammal/Dockerfile .
