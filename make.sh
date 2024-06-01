f0c0c3f4ab64d9bb8ed05a0c42860cb551244528
Ben Evans <beevans@redhat.com>
2024-06-01 18:53:54 +0200

    Fix make.sh

--- make.sh
+++ make.sh
@@ -1,9 +1,9 @@
-#To build a image
+# To build the Maven jar
+mvn clean package
 
+# Build the containers
 docker build  -t animals_demo -f src/main/docker/animal/Dockerfile .
 docker build  -t fish_demo -f src/main/docker/fish/Dockerfile .
 docker build  -t mustelid_demo -f src/main/docker/mustelid/Dockerfile .
 docker build  -t feline_demo -f src/main/docker/feline/Dockerfile .
 docker build  -t mammal_demo -f src/main/docker/mammal/Dockerfile .
-
-
