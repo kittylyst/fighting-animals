a5f77d4770af7add92ebbf425fbd51d6170583af
Ben Evans <beevans@redhat.com>
2024-06-01 18:22:18 +0200

    Make make.sh useful

--- make.sh
+++ make.sh
@@ -1,13 +1,6 @@
-#This Docker file helps to run a animal service
-
-#To build a package
-
-mvn package
-
-#To build a image
-
-docker build -t animalservice
-
-#List a images
-
-docker images
+mvn clean package
+docker build -t animals_demo -f src/main/docker/animal/Dockerfile target/
+docker build -t feline_demo -f src/main/docker/feline/Dockerfile target/
+docker build -t fish_demo -f src/main/docker/fish/Dockerfile target/
+docker build -t mammal_demo -f src/main/docker/mammal/Dockerfile target/
+docker build -t mustelid_demo -f src/main/docker/mustelid/Dockerfile target/
