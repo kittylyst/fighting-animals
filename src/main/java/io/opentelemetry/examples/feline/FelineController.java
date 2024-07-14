d28de52d6c5bdb6951bddbe36bdc55305d438f9c
Ben Evans <benjamin.john.evans@gmail.com>
2024-07-14 08:07:34 +0200

    Initial attempt to add Kafka

--- src/main/java/io/opentelemetry/examples/feline/FelineController.java
+++ src/main/java/io/opentelemetry/examples/feline/FelineController.java
@@ -2,6 +2,8 @@
 package io.opentelemetry.examples.feline;
 
 import java.util.List;
+import java.util.Properties;
+import org.apache.kafka.clients.producer.KafkaProducer;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,11 +11,21 @@ import org.springframework.web.bind.annotation.RestController;
 public class FelineController {
   private final List<String> CATS = List.of("tabby", "jaguar", "leopard");
 
+  private final KafkaProducer<String, String> producer;
+
+  public FelineController() {
+    Properties properties = new Properties();
+    properties.put("bootstrap.servers", "kafka-1:9092"); // PLAINTEXT
+    producer = new KafkaProducer<>(properties);
+  }
+
   @GetMapping("/getAnimal")
   public String makeBattle() throws InterruptedException {
     // Random pause
     Thread.sleep((int) (20 * Math.random()));
     // Return random cat
-    return CATS.get((int) (CATS.size() * Math.random()));
+    var cat = CATS.get((int) (CATS.size() * Math.random()));
+    // producer.send("FELINE", cat);
+    return cat;
   }
 }
